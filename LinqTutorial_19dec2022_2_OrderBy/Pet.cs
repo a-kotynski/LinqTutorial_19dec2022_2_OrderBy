dca2fc9be04a615769cf19e5bbf886da20eed126
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-12-20 01:24:20 +0100

    Add project files.

---
+++ LinqTutorial_19dec2022_2_OrderBy/Pet.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqTutorial_19dec2022_2_OrderBy;
+
+public enum PetType
+{
+    Dog,
+    Cat,
+    Fish
+}
+
+public class PetByTypeComparer : IComparer<Pet>
+{
+    int IComparer<Pet>.Compare(Pet? x, Pet? y)
+    {
+        return x.Type.CompareTo(y.Type);
+    }
+}
+public class PetComparerById : IEqualityComparer<Pet>
+{
+    bool IEqualityComparer<Pet>.Equals(Pet? x, Pet? y)
+    {
+        return x.Id == y.Id;
+    }
+
+    int IEqualityComparer<Pet>.GetHashCode(Pet obj)
+    {
+        return obj.Id;
+    }
+}
+internal class Pet
+{
+    public Pet(int id, string name, PetType type, float number)
+    {
+        Id = id;
+        Name = name;
+        Type = type;
+        Number = number;
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public PetType Type { get; set; }
+    public float Number { get; set; }
+}
---
+++ LinqTutorial_19dec2022_2_OrderBy/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace LinqTutorial_19dec2022_2_OrderBy;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var pets = new[]
+    {
+        new Pet(1, "Hannibal", PetType.Fish, 1.1f),
+        new Pet(2, "Anthony", PetType.Cat, 2f),
+        new Pet(3, "Ed", PetType.Cat, 0.7f),
+        new Pet(4, "Taiga", PetType.Dog, 35f),
+        new Pet(5, "Rex", PetType.Dog, 40f),
+        new Pet(6, "Lucky", PetType.Dog, 5f),
+        new Pet(7, "Storm", PetType.Cat, 0.9f),
+        new Pet(8, "Nyan", PetType.Cat, 2.2f)
+    };
+        var numbers = new[] { 16, 8, 9, -1, 2 };
+        var orderedNumbers = numbers.OrderBy(number => number);
+
+        //Console.WriteLine("\nOrdered Numbers:");
+        //foreach (var number in orderedNumbers)
+        //{
+        //    Console.WriteLine($"{number}");
+        //}
+
+        var words = new[] { "lion", "tiger", "snow leopard" };
+        var orderedWords = words.OrderBy(word => word);
+        
+        //Console.WriteLine("\nOrdered Words:");
+        //foreach (var word in orderedWords)
+        //{
+        //    Console.WriteLine($"{word}");
+        //}
+
+        var petsOrderByName = pets.OrderBy(pet => pet.Name);
+
+        //Console.WriteLine("\nOrdered Pets:");
+        //foreach (var pet in petsOrderByName)
+        //{
+        //    Console.WriteLine(pet.Name);
+        //}
+
+        //Console.WriteLine("\nPets:");
+        //foreach (var pet in pets)
+        //{
+        //    Console.WriteLine(pet.Name);
+        //}
+
+        var petsOrderByIdDesc = pets.OrderByDescending(pet => pet.Id);
+
+        //Console.WriteLine("\nOrdered Pets:");
+        //foreach (var pet in petsOrderByIdDesc)
+        //{
+        //    Console.WriteLine($"{pet.Id}, {pet.Name}");
+        //}
+
+        var petsOrderedByTypeThenName = pets.OrderBy(pet => pet.Type).ThenBy(pet => pet.Name);
+
+        //Console.WriteLine("\nOrdered Pets Twice:");
+        //foreach (var pet in petsOrderedByTypeThenName)
+        //{
+        //    Console.WriteLine($"{pet}");
+        //}
+
+        var petsOrderedByTypeWithComparer = pets.OrderBy(pet => pet, new PetByTypeComparer());
+
+        //Console.WriteLine($"\n{nameof(petsOrderedByTypeWithComparer)}:");
+        //foreach (var pet in petsOrderedByTypeWithComparer)
+        //{
+        //    Console.WriteLine($"{pet.Name}");
+        //}
+
+        var petsReversed = pets.Reverse();
+        Console.WriteLine($"\n{nameof(petsReversed)}:");
+        foreach (var pet in petsReversed)
+        {
+            Console.WriteLine($"{pet.Id}, {pet.Name}");
+        }
+    }
+}
